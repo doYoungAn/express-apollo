98daed93446e51857c9c4c890a5bf2cb8fef3f15
doYoungAn <jispoop@gmail.com>
2020-07-07 00:05:41 +0900

    graphql 파일 임포트

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,24 @@
 import express, { Express } from 'express';
+import bodyParser from 'body-parser';
+import { ApolloServer, gql } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+
+import path from 'path';
+import { loadSchemaSync } from '@graphql-tools/load';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+import { addResolversToSchema } from '@graphql-tools/schema';
+
+import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge';
+import { loadFilesSync } from '@graphql-tools/load-files';
+
+const schema = loadSchemaSync(path.join(__dirname, './graphql/schema.graphql'), {
+    loaders: [new GraphQLFileLoader()],
+});
+const resolvers = { Query: { } };
+
+const a = loadFilesSync([path.join(__dirname, './graphql/schema.graphql')])
+console.log('a', gql(a.join('')))
+// const schema = gql(a.join(''))
 
 class App {
 
@@ -6,6 +26,9 @@ class App {
 
     constructor() {
         this._express = express();
+        this._express.use(bodyParser.json());
+        const server = new ApolloServer({ schema, resolvers });
+        server.applyMiddleware({ app: this._express });
     }
 
     public listening(port: number): Promise<void> {
